e4cbe6a2719c12422d0f076a271627227048d0aa
qinxin <goal_qinxin@163.com>
2021-03-15 22:07:28 +0800

    init

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  var a = 1 
+  function aoo (){
+    console.log(a)
+    // nb = '555'
+  }
+  aoo()
+  return (
+    <div className="App">
+      f
+    </div>
+  );
+}
+
+export default App;
